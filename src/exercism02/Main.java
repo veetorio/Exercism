20461ac43f791421e29fbe8aceda472b5f663b29
Veetorio <ettorevitorio98@gmail.com>
2024-03-16 17:02:34 -0300

    algoritmo de inverter a array

--- src/exercism02/Main.java
+++ src/exercism02/Main.java
@@ -1,10 +1,21 @@
 package exercism02;
 
-import exercism01.service.Lasagna;
-
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String... args){
+        String hello = "hello world";
+        System.out.println(reversed(hello));
 
 
+    }
+
+
+
+    private static String reversed(String array){
+        var reversedArray = "";
+        for(int i = array.length(); 0 < i; i--){
+            reversedArray += array.charAt(i-1);
+        }
+
+        return reversedArray;
     }
 }
