bad543c6f110780fb4ca6dbe8078cfded1fc7f9b
Veetorio <ettorevitorio98@gmail.com>
2024-03-21 13:34:54 -0300

    aff

--- src/exercism04/Main.java
+++ src/exercism04/Main.java
@@ -1,15 +1,16 @@
 package exercism04;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
-        String senha = "4539 3195 0343 6468";
+        String senha = "4539 3195 0343 6467";
         String senha2 = "8273 1232 7352 0569";
-        System.out.println("[8,5,6,9,6,1,9,5,0,3,8,3,3,4,3,7]");
-        System.out.println(validation(senha));
+        System.out.println(validation(senha2));
     }
 
     private static Boolean validation(String args){
@@ -17,7 +18,7 @@ public class Main {
         if(args.length() <= 1 || args.isBlank()) return  false;
         args = args.replaceAll(" ","");
         args = args.replaceAll("(.)","$1.");
-        System.out.println(args);
+
 
 
         List<String> primary = new ArrayList<String>(List.of(args.split("\\.")));
@@ -27,20 +28,18 @@ public class Main {
                 .map(Integer::parseInt)
                 .toList();
 
-        List<Integer> terciary = secundary
-                .stream()
-                .map(item -> {
-                    int index = secundary.indexOf(item);
-                    return index % 2 == 0? item * 2 > 9 ? item * 2 - 9 : item * 2 : item ;
+        long[] terciary = IntStream
+                .range(0,secundary.size())
+                .mapToLong( (i) -> {
+                    var obj = secundary.get(i);
+                    int filter1 = i % 2 == 0 ? obj * 2 : obj;
+                    int filter2 = filter1 > 9 ? filter1 - 9 : filter1;
+                    return filter2;
+
                 })
-                .toList();
+                .toArray();
 
-        Integer sum = terciary
-                .stream()
-                .mapToInt(Integer::intValue)
-                .sum();
-
-        System.out.println(sum);
+        int sum = Math.toIntExact(Arrays.stream(terciary).sum());
 
         return sum % 10 == 0;
     }
